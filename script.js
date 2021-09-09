fee98e0e1f0d2fa66cf8ccf38b948f904e435f86
Lia OBrien <liaobrien123@gmail.com>
2021-09-09 18:21:30 -0400

    added arrays for all character types and prompt for password length

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// all the possible characters a random password can contain
+var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // Write password to the #password input
+
+
+function generatePassword() {
+  // var myPassword
+  // return password;
+  charLength = prompt("How many characters would you like your password to be? Put a number between 8 and 128.");
+  if (!charLength) {
+    alert("Please enter a value between 8 and 128.")
+    return generatePassword();
+  }
+
+}
+
+
+
+function passwordLength() {
+
+}
+
+function specialCharacters() {
+  var specialChar = confirm("Click 'Ok' to include special characters");
+  if (specialChar === true) {
+    numbers();
+  }
+}
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +48,25 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// onclick of some kind, prompt("")
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// confirm("") to say yes or no criteria
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
