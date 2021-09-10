bb880ccc7e13d985e5dadd2aa6bd5b9a298b71f9
Lia OBrien <liaobrien123@gmail.com>
2021-09-09 22:21:22 -0400

    created new variable that concatenated all my arrays and created random password

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 
 
 function generatePassword() {
-  // var myPassword
-  // return password;
   charLength = prompt("How many characters would you like your password to be? Put a number between 8 and 128.");
   if (!charLength) {
     alert("Prompt cannot be left blank.")
@@ -23,16 +21,42 @@ function generatePassword() {
     return generatePassword();
   }
   else if (8 < charLength < 128) {
-    var specialCharResponse = confirm("Click 'Ok' to include special characters.");
-    var numberResponse = confirm("Click 'Ok' to include numbers.");
-    var upperResponse = confirm("Click 'Ok' to include uppercase letters.");
-    var lowerResponse = confirm("Click 'Ok' to include lowercase letters.");
+    var specialCharResponse = confirm("Click 'OK' to include special characters.");
+    var numberResponse = confirm("Click 'OK' to include numbers.");
+    var upperResponse = confirm("Click 'OK' to include uppercase letters.");
+    var lowerResponse = confirm("Click 'OK' to include lowercase letters.");
     if (specialCharResponse === false && numberResponse === false && upperResponse === false && lowerResponse === false) {
-      alert("You must choose at least one type of characters to include.")
+      alert("You must choose at least one group of characters to include.")
     }
   }
 
+  // making a new array combining my global arrays above
+  var passwordCharacters = []
+  if (specialCharResponse) {
+    passwordCharacters = passwordCharacters.concat(specialChars);
+  }
 
+  if (numberResponse) {
+    passwordCharacters = passwordCharacters.concat(numbers);
+  }
+
+  if (upperResponse) {
+    passwordCharacters = passwordCharacters.concat(upperCaseLetters);
+  }
+  if (lowerResponse) {
+    passwordCharacters = passwordCharacters.concat(lowerCaseLetters);
+  }
+
+  console.log(passwordCharacters);
+
+  var randomPassword = ""
+
+  // randomizes the array i made above and translates it into the random password based on character length request
+  for (var i = 0; i < charLength; i++) {
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword);
+  }
+  return randomPassword;
 }
 
 
@@ -45,26 +69,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// onclick of some kind, prompt("")
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// confirm("") to say yes or no criteria
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
